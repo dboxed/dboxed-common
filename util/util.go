49be89fc8062dcf67bdc9a6fb8006b0a8fbdfe57
Alexander Block <ablock84@gmail.com>
2025-08-29 15:33:37 +0200

    Add AtomicWriteFile

--- util/util.go
+++ util/util.go
@@ -1,6 +1,11 @@
 package util
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func Ptr[T any](v T) *T {
 	return &v
@@ -41,4 +46,33 @@ func EqualsViaJson(a any, b any) bool {
 	aj := MustJson(a)
 	bj := MustJson(b)
 	return aj == bj
+}
+
+func AtomicWriteFile(path string, b []byte, perm os.FileMode) error {
+	tmpFile, err := os.CreateTemp(filepath.Dir(path), filepath.Base(path)+".*")
+	if err != nil {
+		return err
+	}
+	defer tmpFile.Close()
+	defer os.Remove(tmpFile.Name())
+
+	_, err = tmpFile.Write(b)
+	if err != nil {
+		return err
+	}
+	err = tmpFile.Close()
+	if err != nil {
+		return err
+	}
+
+	err = os.Chmod(tmpFile.Name(), perm)
+	if err != nil {
+		return err
+	}
+
+	err = os.Rename(tmpFile.Name(), path)
+	if err != nil {
+		return fmt.Errorf("failed to rename file: %w", err)
+	}
+	return nil
 }
